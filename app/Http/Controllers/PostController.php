82bcf4054ea5a9b3962415e18aaeec295fef19f5
zhuma92 <zhumabek.aykyn@mail.ru>
2023-12-17 04:24:59 +0600

    docker init

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
+    public function __invoke()
     {
-        return 'posts222';
+        return 'posts';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('posts',[\App\Http\Controllers\PostController::class,'index']);
+Route::get('/posts',\App\Http\Controllers\PostController::class);
